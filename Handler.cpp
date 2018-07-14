91c0630c993cbf0cfc9dd23daba87943cbc12c2a
Stefano Montesi <dincio.montesi@gmail.com>
2018-07-14 15:47:39 +0200

    Made draw function const. Also changed Handler class' prototype to suit Scrabble's requirements.

--- Handler.cpp
+++ Handler.cpp
@@ -2,27 +2,22 @@
 
 #include <cassert>
 
-/// Handler static variables
-// next handler to be set by change function
+///Handler static variables
+//next handler to be set by change function
 Handler* Handler::mNext = nullptr;
 
-/// Handler functions
-// change handler (static)
-void Handler::change(Handler*& handler)
-{
+///Handler functions
+//change handler (static)
+void Handler::change(Handler*& handler){
     assert(handler);
-    if(mNext != nullptr)
-    {
+    if(mNext != nullptr){
         delete handler;
         handler = mNext;
         mNext = nullptr;
     }
 }
-// set next handler - return false on failure
-bool Handler::setNext(Handler* handler)
-{
-    if(mNext != nullptr || handler == nullptr)
-        return false;
+//set next handler - return false on failure
+bool Handler::setNext(Handler* handler){
+    if(mNext != nullptr || handler == nullptr) return false;
     mNext = handler;
-    return true;
 }
--- Handler.h
+++ Handler.h
@@ -5,33 +5,28 @@
 ///Handler base class
 class Handler{
     public:
-        // constructor
-        Handler(sf::RenderWindow& window): mWindow(&window) {}
-
-        // virtual destructor
-        virtual ~Handler() {}
-
-        // deallocate given handler and change it to point to the next one
+        ///constructor
+        Handler(sf::RenderWindow& window): mWindow(window) {}
+        ///static functions
+        //deallocate given handler and change it to point to the next one
         static void change(Handler*& handler);
-
-        /// setters
-        // sets next handler only if current next handler is null
-        // returns success
+        ///setters
+        //sets next handler only if current next handler is null.
+        //returns success.
         bool setNext(Handler* handler);
-
-        /// interface functions
-        // handle user event
+        ///interface functions
+        //handle user event
         virtual void handleEvent(const sf::Event& e) = 0;
-        // handle logical operations
-        virtual void handleLogic(const sf::Time& time) = 0;
-        // draw interface
-        virtual void handleDrawing() = 0;
-
+        //handle logical operations
+        virtual void handleLogic() = 0;
+        //draw interface
+        virtual void handleDrawing() const = 0;
     protected:
-        // window on which the interface is shown
-        sf::RenderWindow* mWindow;
-
+        ///variables
+        //window on which the interface is shown
+        sf::RenderWindow& mWindow;
     private:
-        // next handler to be used
+        ///static variables
+        //next handler to be used
         static Handler* mNext;
 };
